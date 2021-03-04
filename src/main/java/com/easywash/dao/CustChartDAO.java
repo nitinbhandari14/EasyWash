0241e05b226dfac771d09c0436eda1c14c30af74
nitinbhandari14 <36134629+nitinbhandari14@users.noreply.github.com>
2021-03-04 11:25:51 +0530

    Create CustChartDAO.java

---
+++ src/main/java/com/easywash/dao/CustChartDAO.java
@@ -0,0 +1,19 @@
+package com.easywash.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+@Service
+public class CustChartDAO {
+	
+	/*
+	 * public List<Object, Object> getCanvasjsChartData() {
+	 * 
+	 * 
+	 * return canvasjsChartDao.getCanvasjsChartData(); }
+	 * 
+	 * public void setCanvasjsChartDao(CanvasjsChartDao canvasjsChartDao) {
+	 * this.canvasjsChartDao = canvasjsChartDao; }
+	 */
+}
